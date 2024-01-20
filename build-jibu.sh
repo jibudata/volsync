568e1400a7af0ffdafc4121c0627094e5b3823de
Ning Ding <834652870@qq.com>
2024-01-20 13:37:46 +0800

    build 0.8.1

--- build-jibu.sh
+++ build-jibu.sh
@@ -1,4 +1,4 @@
 export DOCKERFILE="Dockerfile-jibu"
-export IMG="registry.cn-shanghai.aliyuncs.com/jibutech/volsync:0.8.0-jibu"
+export IMG="jibutech-registry.cn-hangzhou.cr.aliyuncs.com/ys1000/volsync:0.8.1-jibu"
 export PLATFORMS="linux/arm64,linux/amd64"
 docker buildx build --platform=${PLATFORMS} -f $DOCKERFILE -t ${IMG} . --push
